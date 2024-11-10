bd12c738d664bffb2bf5e666e16823cc38b1dac7
Nicolas Huang <nsiyuhuang@gmail.com>
2024-11-10 05:12:50 -0500

    UI Changes

--- ripple-ui/vite.config.ts
+++ ripple-ui/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'three',
+      'three/examples/jsm/loaders/GLTFLoader',
+      'three/examples/jsm/utils/SkeletonUtils',
+      '@react-three/fiber',
+      '@react-three/drei'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [/three/, /node_modules/]
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
